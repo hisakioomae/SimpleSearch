96027ce9b48bc4f6fc088db0ef0aa8ec8b1aa491
hisaki.oomae <hisaki0616@gmail.com>
2017-10-15 22:54:17 +0900

    ファイル読み込みに変更

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,44 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-        String text[] = {"a", "b", "c", "a", "b", "c", "a", "b", "a", "b", "c", "a", "b", "a"};
-        String key[] = {"a", "b", "c"};
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
-        for (int i = 0; i < text.length - key.length + 1; i++) {
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+
+        FileChooser fileChooser = new FileChooser();//「ファイルを開く」ダイアログを表示する
+        File file = fileChooser.showOpenDialog(primaryStage);//選択されたファイルパスを取得する
+        FileInputStream fis = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+        String text = br.readLine();
+
+        String key = "look";
+        SimpleSearch(text, key);
+    }
+
+    private static void SimpleSearch(String text, String pattern) {
+        int textLen = text.length();
+        int patnLen = pattern.length();
+        for (int i = 0; i < textLen - patnLen + 1; i++) {
             int k = 0;
-            for (int j = 0; j < key.length; j++) {
+            for (int j = 0; j < patnLen; j++) {
                 k++;
-                if (text[i + j] != key[j]) {
+                if (text.charAt(i+j) != pattern.charAt(j)) {
                     break;
                 }
-                if (k == key.length) // 文字列の長さだけ一致すれば
+                if (k == pattern.length()) // 文字列の長さだけ一致すれば
                     System.out.println(i + 1);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
